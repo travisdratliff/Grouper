6e08fdc0effa4194977aba781c5914d8227b8236
Travis Ratliff <travis.ratliff14@gmail.com>
2026-05-04 17:33:37 -0500

    Initial Commit

---
+++ Grouper/GrouperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrouperApp.swift
+//  Grouper
+//
+//  Created by Travis Domenic Ratliff on 5/4/26.
+//
+
+import SwiftUI
+
+@main
+struct GrouperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
